1bb24bd8901815746b087fbda221f29551813979
Zuhaib Rashid <xuvaxuva006@gmail.com>
2026-08-19 22:23:10 +0530

    we are done

--- client/src/app/sitemap.ts
+++ client/src/app/sitemap.ts
@@ -3,13 +3,13 @@ import { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://heal-os-client.vercel.app',
+      url: 'https://healos-theta.vercel.app',
       lastModified: new Date(),
       changeFrequency: 'yearly',
       priority: 1,
     },
     {
-      url: 'https://heal-os-client.vercel.app/contact',
+      url: 'https://healos-theta.vercel.app/contact',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.8,
